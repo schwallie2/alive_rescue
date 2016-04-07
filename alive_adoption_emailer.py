d619f14d25d3270a1361b83235edc0e8a36a7122
Chase Schwalbach <chase.schwalbach@avant.com>
2016-04-06 23:17:01 -0500

    General build out and updates

--- alive_adoption_emailer.py
+++ alive_adoption_emailer.py
@@ -1,8 +1,9 @@
 import gspread
 import pandas as pd
 from oauth2client.client import SignedJwtAssertionCredentials
-
+# Config relies on secret.py
 import config
+# send_email uses secret.py for logins
 import send_email
 
 
@@ -20,6 +21,7 @@ class AliveEmailer(object):
                                                     config.drive_details['private_key'], scope)
         gc = gspread.authorize(credentials)
         # Open a worksheet from spreadsheet with one shot
+        self.summary_email = send_email.EmailHandler(to=config.master_email, subject='E-mails being sent out today')
         tit = 'Adoption Follow Up Database'
         self.wkbk = gc.open(tit)
         self.adopter = self._get_adopter_info()
@@ -36,20 +38,16 @@ class AliveEmailer(object):
             if row['1 week'] == 'TRUE':  # Need to send, if after 1 week
                 if pd.datetime.today() >= adopt_date + pd.tseries.offsets.Week(1):
                     # This Adopter needs to send out the weekly update
-                    print 'week'
-                    print row
+                    self._send_update_email(idx, row, '1 week')
             if row['1 month'] == 'TRUE':
                 if pd.datetime.today() >= adopt_date + pd.tseries.offsets.Day(30):
-                    print 'month'
-                    print row
+                    self._send_update_email(idx, row, '1 month')
             if row['3 month'] == 'TRUE':
                 if pd.datetime.today() >= adopt_date + pd.tseries.offsets.Day(90):
-                    print '3 month'
-                    print row
+                    self._send_update_email(idx, row, '3 month')
             if row['1 year'] == 'TRUE':
                 if pd.datetime.today() >= adopt_date + pd.tseries.offsets.Day(365):
-                    print 'year'
-                    print row
+                    self._send_update_email(idx, row, '1 year')
 
     def _send_update_email(self, idx, row, type_email):
         """
@@ -68,10 +66,11 @@ class AliveEmailer(object):
         return adopter
 
     def _send_new_email(self, to, subject):
-        self.eh = send_email.EmailHandler(to=to, subject=subject, from_='ALIVE Rescue')
+        self.eh = send_email.EmailHandler(to=to, subject=subject, from_='ALIVE Rescue', user=config.gmail_user,
+                                          pwd=config.gmail_pwd)
         self.eh.add_random_text('''<br><br>Sarah Brewster<br>
                               Director of Adoptions and Foster''')
-        self.eh.send_email(config.gmail_user, config.gmail_pwd)
+        self.eh.send_email()
 
     def _get_email_text(self):
         """
